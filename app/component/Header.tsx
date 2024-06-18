181c913113163e6134687fd254f9bf1021ebee08
진상우 <tkddnrj321@naver.com>
2024-06-18 21:18:39 +0900

    first commit

---
+++ app/component/Header.tsx
@@ -0,0 +1,11 @@
+const Header = () => {
+    return (
+        <header className="flex justify-center items-center w-full m-4">
+            <div>
+                <span>Header is Here.</span>
+            </div>
+        </header>
+    );
+};
+
+export default Header;
---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+    return (
+        <div className="flex m-4">
+            <span>Home is here.</span>
+        </div>
+    );
+}
